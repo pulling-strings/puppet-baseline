92e588c0c6faac3e8c6be196dacd58d3cbab30c2
narkisr <narkisr@gmail.com>
2014-09-18 14:55:33 +0300

    moving syslog to logging

--- manifests/security.pp
+++ manifests/security.pp
@@ -1,5 +1,5 @@
 # Security tools
-class baseline::security($syslog_server='') {
+class baseline::security {
   package{'pwgen':
     ensure => present
   }
@@ -13,23 +13,4 @@ class baseline::security($syslog_server='') {
   package{'truecrypt':
     ensure  => present
   }
-
-  if(!empty($syslog_server)){
-    # order matters!
-    file_line { 'graylog2-rsyslog-template':
-      path => '/etc/rsyslog.conf',
-      line => '$template GRAYLOG2,"<%PRI%>1 %timegenerated:::date-rfc3339% %HOSTNAME% %syslogtag% - %APP-NAME%: %msg:::drop-last-lf%\n"'
-    } ->
-
-    file_line { 'action-forward-default':
-      path => '/etc/rsyslog.conf',
-      line => '$ActionForwardDefaultTemplate GRAYLOG2'
-    } ->
-
-    file_line { 'syslog-dest-server':
-      path => '/etc/rsyslog.conf',
-      line => "*.* @${syslog_server}"
-    }
-  }
-
 }
