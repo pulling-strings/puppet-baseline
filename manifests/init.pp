d7a36fc5e2b4b1d9be3f369029bc51fe5e9211f1
ronen <narkisr@gmail.com>
2014-11-05 13:05:51 +0200

    removing non existing class include

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,6 @@
 # baseline tools and package for a working secured system.
 class baseline($home=false) {
   validate_string($home)
-  include hardening
   include security
   include networking
   include filesystem
