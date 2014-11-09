13a7df9441c0982c4d64b22c016d881045cd6abf
ronen <narkisr@gmail.com>
2014-11-10 00:11:27 +0200

    adding celestial user

---
+++ manifests/celestial.pp
@@ -0,0 +1,27 @@
+# Enabling remote Celesital Puppet run
+class baseline::celestial($user='celestial') {
+
+  baseline::ssh_keys{$user: }
+
+  user{ $user:
+    ensure  => present,
+    comment => 'A Celestial only user',
+    home    => "/home/${user}"
+  } ->
+
+  file{"/home/${user}":
+    ensure => directory,
+    owner  => $user,
+    group  => $user
+  }
+
+  file{'/etc/sudoers.d/celestial':
+    ensure => present
+  } ->
+
+  file_line { 'Celestial Puppet run':
+    path => '/etc/sudoers.d/celestial',
+    line => "${user} ALL=NOPASSWD: /tmp/*/scripts/run.sh --detailed-exitcodes"
+  }
+
+}
