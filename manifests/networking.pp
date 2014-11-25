69afcf5239728e7b53569c2eaecbc72e743768c2
narkisr <narkisr@gmail.com>
2014-11-25 03:08:19 +0200

    adding user/owner

--- manifests/networking.pp
+++ manifests/networking.pp
@@ -1,14 +1,16 @@
 # Basic neworking utilities
-class baseline::networking {
+class baseline::networking($user){
 
   package{['sshuttle','wakeonlan','nmap','aria2']:
     ensure  => present
   }
 
-  ensure_resource('file', "${baseline::home}/.ssh", {'ensure' => 'directory'}) 
+  ensure_resource('file', "${baseline::home}/.ssh", {'ensure' => 'directory'})
 
   file{"${baseline::home}/.ssh/config":
     source  => 'puppet:///modules/baseline/ssh_config',
-    require => File["${baseline::home}/.ssh"]
+    require => File["${baseline::home}/.ssh"],
+    owner   => $user,
+    group   => $user
   }
 }
