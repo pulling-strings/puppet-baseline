4883ec47a04da40fb1ef46d6a2f07c54d9a9cb26
ronen <narkisr@gmail.com>
2015-06-06 01:02:36 +0300

    adding freebsd support

--- manifests/update.pp
+++ manifests/update.pp
@@ -15,26 +15,33 @@ class baseline::update($user='upgrade') {
      group  => $user
   }
 
-  file{'/etc/sudoers.d/upgrade':
+  if $kernel == 'Linux' {
+    $sudoer = '/etc/sudoers.d/upgrade'
+
+    file_line { 'add passwordless update purge':
+     path    => $sudoer,
+     line    => "${user} ALL=NOPASSWD: /usr/bin/apt-get update, /usr/bin/apt-get upgrade -y, /usr/bin/purge-kernels",
+     require => File[$sudoer]
+    }
+
+    file { '/usr/bin/purge-kernels':
+      ensure=> file,
+      mode  => '+x',
+      source=> 'puppet:///modules/baseline/purge-kernels',
+      owner => root,
+      group => root,
+    }
+  } elsif $kernel == 'FreeBSD' {
+    $sudoer = '/usr/local/etc/sudoers.d/upgrade'
+
+    file_line { 'add passwordless update purge':
+     path    => $sudoer,
+     line    => "${user} ALL=NOPASSWD:  /usr/local/bin/pc-updatemanager *"
+     require => File[$sudoer]
+    }
+  }
+
+  file $sudoer {:
     ensure => present
-  } ->
-
-  file_line { 'add passwordless update purge':
-    path => '/etc/sudoers.d/upgrade',
-    line => "${user} ALL=NOPASSWD: /usr/bin/apt-get update, /usr/bin/apt-get upgrade -y, /usr/bin/purge-kernels"
-  }
-
-  file_line { 'remove sudoers file passwordless update':
-    ensure => absent,
-    path   => '/etc/sudoers',
-    line   => "${user} ALL=NOPASSWD: /usr/bin/apt-get update, /usr/bin/apt-get upgrade -y"
-  }
-
-  file { '/usr/bin/purge-kernels':
-    ensure=> file,
-    mode  => '+x',
-    source=> 'puppet:///modules/baseline/purge-kernels',
-    owner => root,
-    group => root,
   }
 }
