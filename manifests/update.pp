f0b2ac5fda20b6169a3f7c77bfd05b070220e942
ronen <narkisr@gmail.com>
2015-06-23 01:50:53 +0300

    adding cleanup and tidying up

--- manifests/update.pp
+++ manifests/update.pp
@@ -4,44 +4,50 @@ class baseline::update($user='upgrade') {
   baseline::ssh::keys{$user: }
 
   user{ $user:
-    ensure      => present,
-    comment     => 'an upgrade only user',
-    home        => "/home/${user}"
+    ensure  => present,
+    comment => 'an upgrade only user',
+    home    => "/home/${user}"
   } ->
 
   file{"/home/${user}":
-     ensure => directory,
-     owner  => $user,
-     group  => $user
+    ensure => directory,
+    owner  => $user,
+    group  => $user
   }
 
-  if $kernel == 'Linux' {
+  if $::kernel == 'Linux' {
     $sudoer = '/etc/sudoers.d/upgrade'
 
+    $update = '/usr/bin/apt-get update'
+    $upgrade = '/usr/bin/apt-get upgrade -y'
+    $clean = '/usr/bin/apt-get clean'
+    $partial = 'rm -rf /var/lib/apt/lists/partial/*'
+    $purge = '/usr/bin/purge-kernels'
+
     file_line { 'add passwordless update purge':
-     path    => $sudoer,
-     line    => "${user} ALL=NOPASSWD: /usr/bin/apt-get update, /usr/bin/apt-get upgrade -y, /usr/bin/purge-kernels",
-     require => File[$sudoer]
+      path    => $sudoer,
+      line    => "${user} ALL=NOPASSWD: ${update}, ${upgrade}, ${clean}, ${partial}, ${purge}",
+      require => File[$sudoer]
     }
 
     file { '/usr/bin/purge-kernels':
-      ensure=> file,
-      mode  => '+x',
-      source=> 'puppet:///modules/baseline/purge-kernels',
-      owner => root,
-      group => root,
+      ensure => file,
+      mode   => '+x',
+      source => 'puppet:///modules/baseline/purge-kernels',
+      owner  => root,
+      group  => root,
     }
-  } elsif $kernel == 'FreeBSD' {
+  } elsif $::kernel == 'FreeBSD' {
     $sudoer = '/usr/local/etc/sudoers.d/upgrade'
 
     file_line { 'add passwordless update purge':
-     path    => $sudoer,
-     line    => "${user} ALL=NOPASSWD:  /usr/local/bin/pc-updatemanager *",
-     require => File[$sudoer]
+      path    => $sudoer,
+      line    => "${user} ALL=NOPASSWD:  /usr/local/bin/pc-updatemanager *",
+      require => File[$sudoer]
     }
   }
 
-  file {$sudoer:
+  file { $sudoer:
     ensure => present
   }
 }
