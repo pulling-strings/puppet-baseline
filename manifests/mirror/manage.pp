78cc1279520c64881cc4148da9134a9d17f84844
ronen <narkisr@gmail.com>
2017-09-15 23:16:42 +1000

    file mode

--- manifests/mirror/manage.pp
+++ manifests/mirror/manage.pp
@@ -6,7 +6,7 @@ class baseline::mirror::manage {
 
     file { "${::baseline::home}/bin/au-mirror":
       ensure  => file,
-      mode    => '0644',
+      mode    => 'u+x',
       source  => 'puppet:///modules/baseline/au-mirror',
       owner   => root,
       group   => root,
@@ -15,7 +15,7 @@ class baseline::mirror::manage {
 
     file { "${::baseline::home}/bin/local-mirror":
       ensure  => file,
-      mode    => '0644',
+      mode    => 'u+x',
       source  => 'puppet:///modules/baseline/local-mirror',
       owner   => root,
       group   => root,
@@ -24,7 +24,7 @@ class baseline::mirror::manage {
 
     file { '/etc/apt/source.list_au':
       ensure  => file,
-      mode    => '0644',
+      mode    => 'u+x',
       content => template('baseline/sources.list.erb'),
       owner   => root,
       group   => root,
