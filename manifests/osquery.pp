b767c90bf62ff309f59f2ec591e24ffc80052e81
ronen <narkisr@gmail.com>
2018-02-25 14:00:30 +1100

    adding osqeury

---
+++ manifests/osquery.pp
@@ -0,0 +1,21 @@
+# Setting up osquery
+class baseline::osquery {
+    apt::source{'osquery':
+      include      => {
+        src => false,
+        deb => true
+      },
+      key          => {
+        id     => '1484120AC4E9F8A1A577AEEE97A80C63C9D8B80B',
+        server => 'keyserver.ubuntu.com'
+      },
+      location     => 'https://osquery-packages.s3.amazonaws.com/xenial',
+      architecture => 'amd64',
+      release      => 'xenial',
+      repos        => 'main',
+    }
+
+    -> package{'osquery':
+      ensure  => present
+    }
+}
