9fe9365cf627a77b7c491aeb15e0699fade06457
ronen <narkisr@gmail.com>
2018-05-11 20:39:25 +1000

    removing duplicated resource

--- manifests/ssh/config.pp
+++ manifests/ssh/config.pp
@@ -1,7 +1,5 @@
 # Misc ssh_config settings
 class baseline::ssh::config($user,$proxies=[]){
-  ensure_resource('file', "${baseline::home}/.ssh", {'ensure' => 'directory'})
-
   $path = "${baseline::home}/.ssh/config"
 
   concat{$path:
